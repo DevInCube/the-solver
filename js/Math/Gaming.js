cc0317cfe86b4a168f0bfde63766289bba859e19
phadyniak <public.hadyniak@gmail.com>
2019-11-26 13:37:41 +0200

    fix proxies

--- js/Math/Gaming.js
+++ js/Math/Gaming.js
@@ -1,48 +1,50 @@
-
 var matrixEl;
 var simplifyMatrix;
 var matrixOutputEl;
 
-var model = {
-	A : new Matrix()
-}
-let modelProxy = null;
+var model = new Proxy({
+	A: new Matrix()
+}, {
+	get(target, key, val) {
+		simplifyMatrix.disabled = !(model.A.valid);
+		Error.check(function () {
+			if (!model.A.valid) Error.add("invalid matrix");
+			if (model.A.valid) {
+				//
+			}
+		});
+		//
+		target[key] = val;
+		return true;
+	}
+});
 
-window.onload = function(){
+window.onload = function () {
 	matrixEl = document.getElementById("matrix");
 	simplifyMatrix = document.getElementById("simplifyMatrix");
 	simplifyMatrix.onclick = simplifyA;
 	matrixOutputEl = document.getElementById("output");
 	Error.init();
-	modelProxy = new Proxy(model, function(e){			
-		simplifyMatrix.disabled = !(model.A.valid);
-		Error.check(function(){			
-			if(!model.A.valid) Error.add("invalid matrix");
-			if(model.A.valid){
-				//
-			}
-		});
-	});
 }
 
-function simplifyA(){	
+function simplifyA() {
 	var mat = new Matrix();
-	mat.copy(model.A);	
-	for(var i = 0; i < mat.height; i++)
+	mat.copy(model.A);
+	for (var i = 0; i < mat.height; i++)
 		mat.items[i][mat.width] = i + 1;
 	var lastLine = [];
-	for(var j =0; j < mat.width + 1;j++)
-		if( j == mat.width)
+	for (var j = 0; j < mat.width + 1; j++)
+		if (j == mat.width)
 			lastLine[j] = NaN;
 		else
-			lastLine[j] = (j+1);
+			lastLine[j] = (j + 1);
 	mat.items[mat.height] = (lastLine);
 	mat.width = mat.width + 1;
 	mat.height = mat.height + 1;
 	matrixOutputEl.value = mat.toString(0);
 }
 
-function simplifyIteration(m){
+function simplifyIteration(m) {
 	/*var sim = false;
 	var remRows = [];	
 	for(var i = 0; i < m.height - 1; i++)
@@ -55,9 +57,9 @@ function simplifyIteration(m){
 }
 
 //matrix input
-function inputChanged(inputEl){
+function inputChanged(inputEl) {
 	var matStr = inputEl.value;
-	if(matStr.slice(-1) == ";") //auto newline
+	if (matStr.slice(-1) == ";") //auto newline
 	{
 		inputEl.value += "\r\n";
 		matStr = inputEl.value;
