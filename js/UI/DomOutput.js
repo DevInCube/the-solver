a873051eda84b54e9e2bac0c528922d583b7ff00
phadyniak <public.hadyniak@gmail.com>
2019-11-28 15:33:38 +0200

    dom output module

--- js/UI/DomOutput.js
+++ js/UI/DomOutput.js
@@ -1,3 +1,24 @@
+function matrixToTable(m) {
+	let digits = 3;
+	let table = document.createElement("table");
+	for (let i = 0; i < m.height; i++) {
+		let row = document.createElement("tr");
+		for (let j = 0; j < m.width; j++) {
+			let cell = document.createElement("td");
+			let val = m.items[i][j];
+			if (!isNaN(val)) {
+				cell.innerHTML = +parseFloat(Math.round(val * 1000) / 1000).toFixed(digits);
+				cell.setAttribute("class", "num");
+			}
+			else
+				cell.innerHTML = "---";
+			row.appendChild(cell);
+		}
+		table.appendChild(row);
+	}
+	return table;
+};
+
 function tableauToTable(t) {
     let rowShift = 2;
     let colShift = 4;
@@ -61,4 +82,41 @@ function tableauToTable(t) {
         table.appendChild(row);
     }
     return table;
+}
+
+function printSimplexLog(log, el) {
+    label(el, "=================================");
+    let fn = "max f(x) = ";
+    for (let i = 0; i < log.problem.width; i++) {
+        if (i > 0) fn += " + ";
+        fn += log.problem.items[0][i] + "&times;X" + (i + 1);
+    }
+    label(el, fn + " ;");
+    for (let it = 0; it < log.iterations.length; it++) {
+        let iteration = log.iterations[it];
+        label(el, "----------------------------");
+        label(el, "Iteration " + it + ":");
+        el.appendChild(tableauToTable(iteration.table));
+        if (iteration.minusRowIndex === -1) {
+            label(el, "=================================");
+            label(el, "No negative elements.");
+            label(el, "x* = ");
+            el.appendChild(matrixToTable(iteration.x));
+            let res = 0;
+            for (let i = 0; i < iteration.table.C.width; i++)
+                res += iteration.table.C.items[0][i] * iteration.x.items[0][i];
+            label(el, "f(x*) = " + res);
+        }
+        else {
+            label(el, "deltas = ");
+            el.appendChild(matrixToTable(iteration.deltas));
+            if (!iteration.gammasData.hasMinusInRow) {
+                label(el, "=================================");
+                label(el, "Has no negatives in negative row. Problem can not be solved.");
+                return; //end
+            }
+            label(el, "gammas = ");
+            el.appendChild(matrixToTable(iteration.gammasData.gammas));
+        }
+    }
 }
